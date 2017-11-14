645f928747cb1fce76e63cb53cbf21b37e0b6a9a
Mostafizur Rahman <mostafizur.cse@gmail.com>
2017-11-15 05:33:20 +0600

    sepia vignette

--- DrawingTest/OpengGL/SimpleFragment.glsl
+++ DrawingTest/OpengGL/SimpleFragment.glsl
@@ -15,6 +15,51 @@ uniform sampler2D TextureTop;
 
 varying vec4 vertTexCoord;
 
+
+const float RADIUS = 0.75;
+vec2 resolution = vec2(900, 1600);
+//softness of our vignette, between 0.0 and 1.0
+const float SOFTNESS = 0.45;
+
+//sepia colour, adjust to taste
+const vec3 SEPIA = vec3(.82, .50, 0.6);
+
+void main() {
+    //sample our texture
+    vec4 texColor = texture2D(Texture, TexCoordOut);
+    
+    //1. VIGNETTE
+    
+    //determine center position
+    vec2 position = (gl_FragCoord.xy / resolution.xy) - vec2(0.5);
+    
+    //determine the vector length of the center position
+    float len = length(position);
+    
+    //use smoothstep to create a smooth vignette
+    float vignette = smoothstep(RADIUS, RADIUS-SOFTNESS, len);
+    
+    //apply the vignette with 50% opacity
+    texColor.rgb = mix(texColor.rgb, texColor.rgb * vignette, 0.5);
+    
+    //2. GRAYSCALE
+    
+    //convert to grayscale using NTSC conversion weights
+    float gray = dot(texColor.rgb, vec3(0.299, 0.587, 0.114));
+    
+    //3. SEPIA
+    
+    //create our sepia tone from some constant value
+    vec3 sepiaColor = vec3(gray) * SEPIA;
+    
+    //again we'll use mix so that the sepia effect is at 75%
+    texColor.rgb = mix(texColor.rgb, sepiaColor, 0.75);
+    
+    //final colour, multiplied by vertex colour
+    gl_FragColor = texColor ;
+}
+
+/*
  float brightness = 1.2;
  float contrast = 0.39;
  float saturation = 0.7;
@@ -31,7 +76,7 @@ void main(void) {
     
     gl_FragColor = vec4(brightness * conColor, 1.0);
 }
-
+*/
 /*
 //consider this as uniforms
 const float modr = 0.3;
