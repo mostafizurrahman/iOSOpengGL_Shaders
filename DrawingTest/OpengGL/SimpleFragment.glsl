492415f2d16479b41216071c430261d34e949572
Mostafizur Rahman <mostafizur.cse@gmail.com>
2017-11-15 05:45:09 +0600

    sketch filter implemented.

--- DrawingTest/OpengGL/SimpleFragment.glsl
+++ DrawingTest/OpengGL/SimpleFragment.glsl
@@ -15,6 +15,34 @@ uniform sampler2D TextureTop;
 
 varying vec4 vertTexCoord;
 
+vec2 resolution = vec2(400, 800);
+const vec4 luminance_vector = vec4(0.3, 0.6, 0.7, 0.0);
+void main() {
+    vec2 uv = vec2(1.0) - (gl_FragCoord.xy / resolution.xy);
+    vec2 n = 1.0/resolution.xy;
+    vec4 CC = texture2D(Texture, uv);
+    vec4 RD = texture2D(Texture, uv + vec2( n.x, -n.y));
+    vec4 RC = texture2D(Texture, uv + vec2( n.x,  0.0));
+    vec4 RU = texture2D(Texture, uv + n);
+    vec4 LD = texture2D(Texture, uv - n);
+    vec4 LC = texture2D(Texture, uv - vec2( n.x,  0.0));
+    vec4 LU = texture2D(Texture, uv - vec2( n.x, -n.y));
+    vec4 CD = texture2D(Texture, uv - vec2( 0.0,  n.y));
+    vec4 CU = texture2D(Texture, uv + vec2( 0.0,  n.y));
+    gl_FragColor = vec4(2.0*abs(length(
+                                       vec2(
+                                            -abs(dot(luminance_vector, RD - LD))
+                                            +4.0*abs(dot(luminance_vector, RC - LC))
+                                            -abs(dot(luminance_vector, RU - LU)),
+                                            -abs(dot(luminance_vector, LD - LU))
+                                            +4.0*abs(dot(luminance_vector, CD - CU))
+                                            -abs(dot(luminance_vector, RD - RU))
+                                            )
+                                       )-0.5));//.5 is pixel area
+}
+
+
+/*
 
 const float RADIUS = 0.75;
 vec2 resolution = vec2(900, 1600);
@@ -57,7 +85,7 @@ void main() {
     
     //final colour, multiplied by vertex colour
     gl_FragColor = texColor ;
-}
+}*/
 
 /*
  float brightness = 1.2;
