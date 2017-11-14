86e3bb4ee3c23a368bf6d9fa846de72d2453f022
Mostafizur Rahman <mostafizur.cse@gmail.com>
2017-11-15 04:20:57 +0600

    filter : biliteral is working fine

--- DrawingTest/OpengGL/SimpleFragment.glsl
+++ DrawingTest/OpengGL/SimpleFragment.glsl
@@ -1,3 +1,9 @@
+
+#ifdef GL_ES
+precision mediump float;
+precision mediump int;
+#endif
+
 varying lowp vec4 DestinationColor; // 1
 
 
@@ -7,26 +13,79 @@ uniform sampler2D TextureFloor;
 uniform sampler2D TextureTop;
 
 
-void main(void) { // 2
+#define SIGMA 5.0
+#define BSIGMA 0.1
+#define MSIZE 10
+
+
+
+
+
+
+// viewport resolution (in pixels)
+//uniform sampler2D texture;          // input channel. XX = 2D/Cube
+
+//uniform float sigma;
+
+
+float normpdf(in float x, in float sigma)
+{
+    return 0.39894*exp(-0.5*x*x/(sigma*sigma))/sigma;
+}
+
+float normpdf3(in vec3 v, in float sigma)
+{
+    return 0.39894*exp(-0.5*dot(v,v)/(sigma*sigma))/sigma;
+}
+
+
+void main(void)
+{
+    float sigma = SIGMA;
+    vec2 resolution = vec2(400, 650);
+    //vec3 c = texture2D(texture, vec2(0.0, 1.0)-(gl_FragCoord.xy / resolution.xy)).rgb;
+    vec2 t = gl_FragCoord.xy / resolution.xy;
+    t.y = 1.0-t.y;
+    vec3 c = texture2D(Texture, t).rgb;
     
-    lowp vec4 S = texture2D(Texture, TexCoordOut);
-    lowp vec4 D = texture2D(TextureFloor, TexCoordOut);;
-    lowp float Da = DestinationColor.a;
-    lowp float Sa = S.a;
-    lowp vec4 R = D*(1.0 - Sa);
-    lowp vec4 B =  texture2D(TextureTop, TexCoordOut);
     
-    if(R.a < 1.0){
-        gl_FragColor = B;
-    } else {
-        gl_FragColor = R;
+    //declare stuff
+    const int kSize = (MSIZE-1)/2;
+    float kernel[MSIZE];
+    vec3 final_colour = vec3(0.0);
+    
+    //create the 1-D kernel
+    float Z = 0.0;
+    for (int j = 0; j <= kSize; ++j)
+    {
+        kernel[kSize+j] = kernel[kSize-j] = normpdf(float(j), sigma);
     }
     
+    
+    vec3 cc;
+    float factor;
+    float bZ = 1.0/normpdf(0.0, BSIGMA);
+    //read out the texels
+    for (int i=-kSize; i <= kSize; ++i)
+    {
+        for (int j=-kSize; j <= kSize; ++j)
+        {
+            vec2 tt = (gl_FragCoord.xy+vec2(float(i),float(j))) / resolution.xy;
+            tt.y = 1.0-tt.y;
+            cc = texture2D(Texture, tt).rgb;
+            factor = normpdf3(cc-c, BSIGMA)*bZ*kernel[kSize+j]*kernel[kSize+i];
+            Z += factor;
+            final_colour += factor*cc;
+            
+        }
+    }		
+    
+    gl_FragColor = vec4(final_colour/Z, 1.0);
     
 }
 
 
-//keep in mind:
-//background is Destination -> AKA 'D'
-//foreground is Source -> AKA 'S'
-//Source is Stroke brush in this context
+/*
+ 
+ 
+ */
