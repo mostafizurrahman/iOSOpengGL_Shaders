2f1d5105a8ba79d61215f0184dcb85feec87de53
Mostafizur Rahman <mostafizur.cse@gmail.com>
2017-11-16 02:50:03 +0600

    sobel effect

--- DrawingTest/OpengGL/SimpleFragment.glsl
+++ DrawingTest/OpengGL/SimpleFragment.glsl
@@ -14,8 +14,41 @@ uniform sampler2D TextureTop;
 
 
 varying vec4 vertTexCoord;
+/*
+float 		width = 400.0;
+float 		height = 400.0 ;
 
-vec2 resolution = vec2(400, 800);
+void make_kernel(inout vec4 n[9], sampler2D tex, vec2 coord)
+{
+    float w = 1.0 / width;
+    float h = 1.0 / height;
+    
+    n[0] = texture2D(tex, coord + vec2( -w, -h));
+    n[1] = texture2D(tex, coord + vec2(0.0, -h));
+    n[2] = texture2D(tex, coord + vec2(  w, -h));
+    n[3] = texture2D(tex, coord + vec2( -w, 0.0));
+    n[4] = texture2D(tex, coord);
+    n[5] = texture2D(tex, coord + vec2(  w, 0.0));
+    n[6] = texture2D(tex, coord + vec2( -w, h));
+    n[7] = texture2D(tex, coord + vec2(0.0, h));
+    n[8] = texture2D(tex, coord + vec2(  w, h));
+}
+
+void main(void)
+{
+    vec4 n[9];
+    make_kernel( n, Texture, TexCoordOut.st );
+    
+    vec4 sobel_edge_h = n[2] + (2.0*n[5]) + n[8] - (n[0] + (2.0*n[3]) + n[6]);
+    vec4 sobel_edge_v = n[0] + (2.0*n[1]) + n[2] - (n[6] + (2.0*n[7]) + n[8]);
+    vec4 sobel = sqrt((sobel_edge_h * sobel_edge_h) + (sobel_edge_v * sobel_edge_v));
+    
+    gl_FragColor = vec4( 1.0 - sobel.rgb, 1.0 );
+}
+
+
+*/
+vec2 resolution = vec2(400, 400);
 const vec4 luminance_vector = vec4(0.3, 0.6, 0.7, 0.0);
 void main() {
     vec2 uv = vec2(1.0) - (gl_FragCoord.xy / resolution.xy);
@@ -46,6 +79,8 @@ void main() {
 }
 
 
+ 
+ 
 /*
 
 const float RADIUS = 0.75;
